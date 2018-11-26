b50106baa21154cc40295e7937e2894ac4ebc0a8
MJ <migueljesuscolom@iespaucasesnoves.cat>
2018-11-26 17:50:59 +0100

    estructura bbdd i disseny web

---
+++ formulari.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script type="text/javascript" src="js/jquery-3.3.1.slim.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/popper.min.js"></script>
+</head>
+<body>
+<div class="">
+
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="js/jquery-3.3.1.slim.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/popper.min.js"></script>
+	<title>Index</title>
+</head>
+<body>
+<div id="carouselId" class="carousel slide anchura" data-ride="carousel">
+    <ol class="carousel-indicators">
+    <li data-target="#carouselId" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselId" data-slide-to="1"></li>
+    <li data-target="#carouselId" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+            <img src="img/allotjament/ODR97F0.jpg" alt="First slide">
+        </div>
+        <div class="carousel-item">
+            <img src="img/allotjament/2854.jpg" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+            <img src="img/allotjament/3034.jpg" alt="Third slide">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselId" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselId" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+<div>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fiked-top">
+    <ul class="nav navbar-nav">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Nav 1 <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Nav 2</a>
+        </li>
+        <li class="nav-item">
+            <form class="form-inline align-content-right">
+                <input type="text" id="buscador" name="buscador">
+                <button type="button" name="bBuscador" class="btn btn-primary">Buscar</button>
+            </form>
+        </li>
+        <li class="nav-item">
+            <button type="submit" class="btn btn-info">Login</button>
+        </li>
+
+    </ul>
+</nav>
+    
+</div>
+
+<div id="destacados" class="form-check">
+<label class="form-check-label">
+    <input type="checkbox" class="form-check-input" name="" id="" value="checkedValue" checked>
+    Display value
+</label>
+</div>">
+
+</div>
+<div id="generar">
+
+</div>
+
+</body>
+</html>
